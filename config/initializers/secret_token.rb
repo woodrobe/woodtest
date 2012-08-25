1de761a35db1ddc77764bfa597e3eec5838301d6
Robert Wood <woodrobe@gmail.com>
2012-08-25 10:43:14 -0600

    First one

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Woodtest::Application.config.secret_token = '870955c692569dfdb65aab353be7891b09564b7c7101d2ba72776727c90b75e43f889ca25f500eb3747bb377343d80b3b670292ed3e264f62c21c48da939e042'
